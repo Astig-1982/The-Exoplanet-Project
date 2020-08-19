d31d1874ef3207717db5bb299923c6f791f1ad06
Astig-1982 <astig.mandalian2014@gmail.com>
2020-08-19 21:07:01 +0000

    Connected with MongoDB

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
+
+app.config["MONGODB_NAME"] =  'other_worlds'
+app.config["MONGO_URI"] = 'mongodb+srv://as_cluster_2020:hello_task_boulevard@myfirstcluster.080hu.mongodb.net/other_worlds?retryWrites=true&w=majority'
+
+
+mongo = PyMongo(app)
+
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    return 'Hello, world'
+@app.route('/exoplanets_display')
+def exoplanets_display():
+    render_template('exoplanets.html', exoplanets=mongo.db.exoplanets.find())
 
 
 if __name__ == '__main__':
