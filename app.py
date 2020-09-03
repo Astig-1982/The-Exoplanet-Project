8071be6e2a557315bd58f41a6d60a652c561d9d1
Astig-1982 <astig.mandalian2014@gmail.com>
2020-09-03 20:27:19 +0000

    Created function to delete exoplanet from exoplanet favourites list

--- app.py
+++ app.py
@@ -32,11 +32,24 @@ def gas_giants_planets():
     return render_template('gas_giants_planets.html', gas_giants_planets=gas_giants_planets)
 
 
+
 @app.route('/add_favourites/<exoplanet_id>')
 def add_favourites(exoplanet_id):
     favourite=mongo.db.exoplanets.find_one({"_id": ObjectId(exoplanet_id)})
     mongo.db.favourites.insert(favourite)
     return render_template('add_favourites.html', favourite_exoplanets=mongo.db.favourites.find())
+
+
+@app.route('/favourite_list')
+def favourite_list():
+    return render_template('add_favourites.html', favourite_exoplanets=mongo.db.favourites.find())
+
+
+@app.route('/delete_favourite/<exoplanet_id>')
+def delete_favourite(exoplanet_id):
+    mongo.db.favourites.remove({"_id": ObjectId(exoplanet_id)})
+    return render_template('add_favourites.html', favourite_exoplanets=mongo.db.favourites.find())
+    
 
 
 if __name__ == '__main__':
