f9daaa700708d3456d75fb742014fd7514c19f28
Astig-1982 <astig.mandalian2014@gmail.com>
2020-09-07 18:54:38 +0000

    Created view for home page

--- app.py
+++ app.py
@@ -20,6 +20,11 @@ def base():
     return render_template('base.html')
 
 
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+
 
 @app.route('/exoplanets_display')
 def exoplanets_display():
