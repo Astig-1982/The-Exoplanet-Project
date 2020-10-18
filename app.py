6cfbf60149d5c33e346360523b65b66a580ef779
Astig-1982 <astig.mandalian2014@gmail.com>
2020-10-18 17:37:36 +0000

    Created 'profile' view to display the profile's page

--- app.py
+++ app.py
@@ -49,6 +49,7 @@ def register():
             # put the new user into 'session' cookie
             session["user"] = request.form.get("username").lower()
             flash("Registration succesful!")
+            return redirect(url_for('profile', username=session["user"])) 
     return render_template('register.html')
 
 
@@ -62,6 +63,7 @@ def login():
           if check_password_hash(existing_user["password"], request.form.get("password")):
               session["user"] = request.form.get("username").lower()
               flash("Welcome, {}".format(request.form.get("username")))
+              return redirect(url_for('profile', username=session["user"]))
           else:
               #invalid password match
               flash("Incorrect Username and/or Password")
@@ -72,6 +74,14 @@ def login():
           flash("Incorrect Username and/or Password")
           return redirect(url_for('login'))
     return render_template('login.html')
+
+
+@app.route('/profile/<username>', methods=['GET', 'POST'])
+def profile(username):
+        username = mongo.db.users.find_one(
+            {"username": session["user"]})["username"]
+        
+        return render_template('profile.html', username=username)
 
 
 @app.route('/exoplanets_display') 
