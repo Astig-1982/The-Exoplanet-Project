c13d37e9cf751d2c2afa698c5cfc58c7429f8d2e
Astig-1982 <astig.mandalian2014@gmail.com>
2020-10-18 16:52:36 +0000

    Created 'login' view

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def home():
     return render_template('home.html')
 
 
-@app.route("/register", methods=["GET", "POST"])
+@app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == "POST":
         #check if the username already exists in the database
@@ -50,6 +50,28 @@ def register():
             session["user"] = request.form.get("username").lower()
             flash("Registration succesful!")
     return render_template('register.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == "POST":
+        existing_user = mongo.db.users.find_one({"username": request.form.get("username").lower()})
+
+        if existing_user:
+          #ensures hashed password matches user input
+          if check_password_hash(existing_user["password"], request.form.get("password")):
+              session["user"] = request.form.get("username").lower()
+              flash("Welcome, {}".format(request.form.get("username")))
+          else:
+              #invalid password match
+              flash("Incorrect Username and/or Password")
+              return redirect(url_for('login'))
+
+        else:
+          #username doesn't exist
+          flash("Incorrect Username and/or Password")
+          return redirect(url_for('login'))
+    return render_template('login.html')
 
 
 @app.route('/exoplanets_display') 
