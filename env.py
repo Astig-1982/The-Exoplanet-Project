cefbf8d418749b561f921721aa3498247a6207a2
Astig-1982 <astig.mandalian2014@gmail.com>
2020-11-06 16:08:43 +0000

    Removed sensitive information about the developer

--- env.py
+++
@@ -1,4 +0,0 @@
-import os
-os.environ["MONGO_URI"] = 'mongodb+srv://as_cluster_2020:hello_task_boulevard@myfirstcluster.080hu.mongodb.net/other_worlds?retryWrites=true&w=majority'
-os.environ["SECRET_KEY"] = 'this_key_is_secret'
-os.environ["DEBUG"] = "1"
